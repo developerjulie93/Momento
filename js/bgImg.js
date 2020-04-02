19dd3e8ff03070e985fce0226c889b90b71a1b07
Juhee Kang <56218979+developerjulie93@users.noreply.github.com>
2020-04-01 23:36:05 -0700

    Add files via upload

--- js/bgImg.js
+++ js/bgImg.js
@@ -4,7 +4,7 @@ const numOfImg = 3;
 function makeBg(){
     const img  = new Image();
     const num = Math.floor(Math.random()*numOfImg)+1;
-    img.src = `img/${num}.jpg`;
+    img.src = `img/${num}.webp`;
     img.classList.add("bgImg");
     body.prepend(img);
 }
--- js/weather.js
+++ js/weather.js
@@ -1,45 +1,49 @@
-const weather = document.querySelector(".weather");
+const weathers = document.querySelector("#jsWeather"),
+        firstChild = weathers.querySelector("#icon_temp"),
+        secondChild = weathers.querySelector("#city");
 
 const COORDS = "coords";
 const API_KEY = "c977126c42ab7bf96050475f273d5231";
+
 function getWeather(lat, lon){
     fetch(
         `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-        ).then(function (response){
-            return response.json();
-        })
-        .then(function (json){
-            const temp = json.main.temp;
-            const place = json.name;
-            weather.innerHTML = `${temp} & ${place}`;
-        });
+    ).then(function (response){
+        return response.json();
+    }).then(function (json){
+        const temp = json.main.temp;
+        const city = json.name;
+        const icon = json.weather[0].icon;
+        firstChild.innerHTML = `${icon} & ${temp}`;
+        secondChild.innerHTML = `${city}`;
+    });
 }
-function handleGeoSuccess(position){
+function handleSuccess(position){
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    const coordsObj = {
+    const coordObj = {
         latitude: latitude,
         longitude: longitude,
     };
-    localStorage.setItem("COORDS", JSON.stringify(coordsObj));
+    localStorage.setItem(COORDS, JSON.stringify(coordObj));
     getWeather(latitude, longitude);
 }
-function handleGeoError(){
-    console.log("Can't access geo location");
+function handleError(){
+    console.log("Can't find current position.");
 }
 function createCoords(){
-    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+    navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
 }
 function loadCoords(){
-    const loadCoords = localStorage.getItem("COORDS");
-    if(loadCoords === null){
+    const loadCoord = localStorage.getItem(COORDS);
+    if(loadCoord === null){
         createCoords();
     }else{
-        const parsedCoords = JSON.parse(loadCoords);
+        const parsedCoords = JSON.parse(loadCoord);
         getWeather(parsedCoords.latitude, parsedCoords.longitude);
-
     }
 }
+
 function init(){
     loadCoords();
 }
